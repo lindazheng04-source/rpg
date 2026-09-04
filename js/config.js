72a460e506e419169f5f1f3bab688a0a4d2e2ce5
lindazheng04-source <lindazheng04@gmail.com>
2026-09-04 14:17:48 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// 静态数据与房屋建造参数配置
+const roomNames = {
+  kitchen: "厨房",
+  bedroom: "睡房",
+  classroom: "课堂",
+  petcorner: "动物游乐角"
+};
+
+const roomCosts = {
+  kitchen: { wood: 20, grass: 0, stone: 10 },
+  bedroom: { wood: 30, grass: 20, stone: 0 },
+  classroom: { wood: 40, grass: 0, stone: 30 },
+  petcorner: { wood: 0, grass: 50, stone: 30 }
+};
